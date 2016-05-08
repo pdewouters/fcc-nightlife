a9135be7b2e540b84846f21ca05ff4242a256c5a
Paul de Wouters <paul@hmn.md>
2016-05-08 15:08:39 +0100

    remove hot middleware

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const app = Server.app()
 if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('./webpack.dev.config.js')
   const compiler = webpack(config)
 
-  app.use(webpackHotMiddleware(compiler))
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
